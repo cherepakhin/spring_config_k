148b45d9b690efca8e68bf8c314a819d006ad1ce
Черепахин Василий <vasi.che@gmail.com>
2024-03-11 13:50:37 +0500

    ALL test OK. Added  ConfigFromCodeRestTest

--- src/test/kotlin/ru/perm/v/springconfig_k/ConfigFromCodeRestTest.kt
+++ src/test/kotlin/ru/perm/v/springconfig_k/ConfigFromCodeRestTest.kt
@@ -12,7 +12,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Disabled
 class ConfigFromCodeRestTest {
     @LocalServerPort
     val port = 8780
