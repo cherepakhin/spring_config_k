7b99903b9c7b33218074903fab15cb77fb2932b1
Черепахин Василий <vasi.che@gmail.com>
2024-03-11 21:15:59 +0500

    2 way testing with MockMvc

--- src/test/kotlin/ru/perm/v/springconfig_k/HelloRestMockMvcTest.kt
+++ src/test/kotlin/ru/perm/v/springconfig_k/HelloRestMockMvcTest.kt
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultMatcher
-import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import ru.perm.v.springconfig_k.rest.HelloRest
@@ -17,11 +17,19 @@ class HelloRestMockMvcTest {
     lateinit var mvc: MockMvc
 
     @Test
-    fun getHello() {
+    fun checkStatusAndResultWithExpect() {
         mvc.perform(get("/hello/"))
             .andExpect(status().isOk)
-            .andExpect(ResultMatcher {
-                result -> assertEquals("hello", result.response.contentAsString)
+            .andExpect(ResultMatcher { result ->
+                assertEquals("hello", result.response.contentAsString)
             })
+    }
+
+    @Test
+    fun checkStatusAndResultFromResult() {
+        val result = mvc.perform(get("/hello/"))
+
+        assertEquals(HttpStatus.OK.value(), result.andReturn().response.status)
+        assertEquals("hello", result.andReturn().response.contentAsString)
     }
 }
